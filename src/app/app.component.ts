1eea7f66d1a10e8f72ac42e96e6156b589f0704e
oyushche <uhmateush@gmail.com>
2020-10-15 07:17:34 -0400

    + requests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,92 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'angular-server-mock';
+export class AppComponent implements OnInit {
+
+  users:User[] = [];
+  sections:Section[] = [];
+  notes:Note[] = [];
+
+  constructor(private http:HttpClient) {
+  }
+
+  ngOnInit(): void {
+
+    this.refreshData();
+
+  }
+
+  refreshData()
+  {
+      this.http.get<User[]>("api/users").subscribe(users => this.users = users);
+      this.http.get<Section[]>("api/sections").subscribe(sections => this.sections = sections);
+      this.http.get<Note[]>("api/notes").subscribe(notes => this.notes = notes);
+  }
+
+  addUser()
+  {
+    this.http.post("/api/users", { name: "new user " + (this.users.length + 1) })
+      .subscribe(() => this.refreshData());
+
+  }
+
+  addSection()
+  {
+    this.http.post("/api/sections",
+      { title: "new section " + (this.sections.length + 1),
+              user: "Oleg"
+      })
+      .subscribe(() => this.refreshData());
+
+  }
+
+  addNote()
+  {
+    this.http.post("/api/notes",
+      { text: "new note " + (this.notes.length + 1),
+        section: "Home"
+      })
+      .subscribe(() => this.refreshData());
+
+  }
+
+  updateNote()
+  {
+    this.http.put("/api/notes", { id: 6, text: "new note " + Math.random() })
+      .subscribe(() => this.refreshData());
+
+  }
+
+  deleteNote(id: number)
+  {
+    this.http.delete("/api/notes/" + id)
+      .subscribe(() => this.refreshData());
+  }
+
+
+
+}
+
+export interface User
+{
+  id: number,
+  name: string
+}
+
+export interface Section
+{
+  id: number,
+  title: string,
+  user: string
+}
+
+export interface Note
+{
+  id: number,
+  text: string,
+  section: string
 }
--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -10,44 +10,34 @@ export class InMemoryDataService implements InMemoryDbService {
 
   createDb() {
 
-
-
     const users = [
-      { id: 1, user: 'oleg' },
-      { id: 1, user: 'irina' }
+      { id: 1, name: 'Oleg' },
+      { id: 2, name: 'Irina' }
     ];
 
     const sections = [
-      {  id: 1, section: 'Work', user: 'irina' },
-      {  id: 2, section: 'Walk', user: 'irina'  },
-      {  id: 3, section: 'Home', user: 'irina'  }
+      {  id: 3, title: 'Work', user: 'Irina' },
+      {  id: 4, title: 'Walk', user: 'Irina'  },
+      {  id: 5, title: 'Home', user: 'Irina'  }
     ];
 
     const notes = [
-      { id: 11, name: 'Dr Nice', section: 'Work' },
-      { id: 12, name: 'Narco', section: 'Work' },
-      { id: 13, name: 'Bombasto', section: 'Work' },
-      { id: 14, name: 'Celeritas', section: 'Work' },
-      { id: 15, name: 'Magneta', section: 'Work' },
-      { id: 16, name: 'RubberMan', section: 'Work' },
-      { id: 17, name: 'Dynama', section: 'Work' },
-      { id: 18, name: 'Dr IQ', section: 'Home' },
-      { id: 19, name: 'Magma', section: 'Home' },
-      { id: 20, name: 'Tornado', section: 'Home' },
+      { id: 6, text: 'Dr Nice', section: 'Work' },
+      { id: 7, text: 'Narco', section: 'Work' },
+      { id: 8, text: 'Bombasto', section: 'Work' },
+      { id: 9, text: 'Celeritas', section: 'Work' },
+      { id: 10, text: 'Magneta', section: 'Work' },
+      { id: 11, text: 'RubberMan', section: 'Work' },
+      { id: 12, text: 'Dynama', section: 'Work' },
+      { id: 13, text: 'Dr IQ', section: 'Home' },
+      { id: 14, text: 'Magma', section: 'Home' },
+      { id: 15, text: 'Tornado', section: 'Home' },
     ];
 
     return {notes, sections, users};
   }
 
-  genId(notes: Note[]): number {
+  genId(): number {
     return this.id++;
   }
-
-}
-
-interface Note
-{
-id: number,
-  name: string,
-  section: string
 }
